e92300f8f161d2c5067e5685e4bb3d4db2bea2e3
Eliza Sousa <eli.sinais@gmail.com>
2024-03-08 15:50:19 -0300

    Implementar função que retorna objeto com os dados do animal velho cuidado por um colaborador

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const foundSpecie = data.employees
+    .find((employee) => employee.id === id).responsibleFor
+    .find((specie) => specie);
+
+  const oldestFound = data.species
+    .find((specie) => specie.id === foundSpecie).residents
+    .reduce((acc, resident) => ((acc.age > resident
+      .age) ? acc : resident));
+
+  return Object.values(oldestFound);
 };
 
 module.exports = getOldestFromFirstSpecies;
