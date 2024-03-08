e83eb43ae99d68846e3b30454e6282d1a0ae8da1
Eliza Sousa <eli.sinais@gmail.com>
2024-03-08 11:12:58 -0300

    Implementar função que retorna os dados de funcionário localizado pelo nome

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees.find((employee) => employee
+    .firstName.includes(employeeName) || employee.lastName
+    .includes(employeeName));
 };
 
 module.exports = getEmployeeByName;
