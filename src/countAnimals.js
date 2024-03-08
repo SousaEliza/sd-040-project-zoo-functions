67bddc6ecc23c2c88b54b436d857ff1f85732431
Eliza Sousa <eli.sinais@gmail.com>
2024-03-08 15:40:35 -0300

    Implementar função que contabiliza espécies

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return species
+      .reduce((acc, specie) => ({ ...acc,
+        [specie.name]: specie.residents
+          .length }), {});
+  }
+  let result;
+  if (!animal.sex) {
+    result = species.find(({ name }) => animal
+      .species.includes(name)).residents;
+  } else {
+    result = species.find(({ name }) => animal
+      .species.includes(name)).residents.filter((resident) => animal.sex === resident.sex);
+  }
+  return result.reduce((acc) => (acc + 1), 0);
 };
 
 module.exports = countAnimals;
