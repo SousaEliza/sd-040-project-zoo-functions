4810e04810a7dc4ec6e66b8d683011a2510a2cd8
Eliza Sousa <eli.sinais@gmail.com>
2024-03-08 15:44:28 -0300

    Implementar função que calcula o valor total da entrada de visitantes

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((visitante) => visitante.age < 18).reduce((acc) => acc + 1, 0);
+  const adult = entrants
+    .filter((visitante) => visitante.age >= 18 && visitante.age < 50)
+    .reduce((acc) => acc + 1, 0);
+  const senior = entrants.filter((visitante) => visitante.age >= 50).reduce((acc) => acc + 1, 0);
+  return {
+    child,
+    adult,
+    senior,
+  };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  const fatores = [];
+  if (!entrants) {
+    return 0;
+  }
+  const entradas = countEntrants(entrants);
+  if (!entradas) {
+    return 0;
+  }
+  fatores.push(entradas, data.prices);
+  return fatores.reduce((total, key) => {
+    const result = (total.child * key.child) + (total
+      .adult * key.adult) + (total.senior * key.senior);
+    return Number(result.toFixed(2));
+  });
 };
 
 module.exports = { calculateEntry, countEntrants };
