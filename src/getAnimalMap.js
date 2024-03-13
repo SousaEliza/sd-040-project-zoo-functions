f0aebbfb267bf6d849b06f012e6dc6664ae6bf2b
Eliza Sousa <eli.sinais@gmail.com>
2024-03-13 10:36:21 -0300

    Implementar a função getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,38 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+function filterByLocation(location) {
+  return species
+    .filter((specie) => specie
+      .location === location);
+}
+
+function filterBySex(sex, residents) {
+  return sex ? residents
+    .filter((resident) => resident
+      .sex === sex) : residents;
+}
+
+function getAnimalNames(includeNames, sorted, key, residents) {
+  if (includeNames) {
+    const residentNames = residents.map((resident) => resident.name);
+    if (sorted) {
+      residentNames.sort((a, b) => a.localeCompare(b));
+    }
+    return { [key]: residentNames };
+  }
+  return key;
+}
+
+const getAnimalMap = (options = {}) => {
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  return locations.reduce((acc, location) => {
+    acc[location] = filterByLocation(location)
+      .map((specie) => {
+        const residents = filterBySex(options.sex, specie.residents);
+        return getAnimalNames(options.includeNames, options.sorted, specie.name, residents);
+      });
+    return acc;
+  }, {});
 };
 
 module.exports = getAnimalMap;
